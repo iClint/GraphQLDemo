cffec1e6c289771d6c41dcf53ac7902ba74c7f01
clintkingston <clintkingston@gmail.com>
2024-02-09 14:42:46 +1100

    update git ignore

---
+++ Enums/ReturnResult.cs
@@ -0,0 +1,8 @@
+namespace GraphQlDemo.Enums;
+
+public enum ReturnResult
+{
+    Successful,
+    NotFound,
+    Failed
+}
---
+++ Models/MongoDbSettings.cs
@@ -0,0 +1,7 @@
+namespace GraphQlDemo.Models;
+
+public class MongoDbSettings
+{
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
+}
